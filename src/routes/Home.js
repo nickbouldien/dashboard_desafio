fb761b3ba4bd381547deedc33103a247c843a0c6
nick <nick.bouldien@gmail.com>
2017-11-18 11:43:43 -0600

    add flow, work on weather feature

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,12 +9,12 @@ const Home = () => (
     <h3>Links to the routes:</h3>
 
     <ListGroup>
-      {/* <ListGroupItem><Link to="portal">portal</Link></ListGroupItem>
-      <ListGroupItem><Link to="return-array">return array</Link></ListGroupItem>
-      <ListGroupItem><Link to="error-boundary">error boundary</Link></ListGroupItem>
-      <ListGroupItem><Link to="custom-attributes">custom attributes</Link></ListGroupItem> */}
+
       <ListGroupItem><Link to='about'>about</Link></ListGroupItem>
       <ListGroupItem><Link to='test'>test route</Link></ListGroupItem>
+
+      <ListGroupItem><a href='https://flow.org/en/docs/react/components/' target="_blank">flow</a></ListGroupItem>
+
     </ListGroup>
   </div>
 );
--- src/routes/Test.js
+++ src/routes/Test.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Image } from 'react-bootstrap';
@@ -8,13 +9,13 @@ class Test extends Component {
     super()
     this.state = {
       wookieFormat: false,
+      weather: null,
       data: null,
-      randomNum: null
+      randomNum: 0
     }
     this.onWookieeVersionClick = this.onWookieeVersionClick.bind(this);
-    this.refresh = this.refresh.bind(this);
+    // this.refresh = this.refresh.bind(this);
     this.callSwapi = this.callSwapi.bind(this);
-    this.mapWookieVersion = this.mapWookieVersion.bind(this);
   }
 
   componentDidMount() {
@@ -27,28 +28,21 @@ class Test extends Component {
     }
   }
 
-  refresh() {
-    window.location.reload();
-  }
+  // refresh() {
+  //   window.location.reload();
+  // }
 
-  callSwapi(changeCharacter) {
-    // hardcoding max/min for now (future allow user to change (w/ slider?) ?) nb???
-    let max = 4;
-    let min = 1;
-    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  callSwapi(changeCity) {
 
-    if (changeCharacter) {
-        randomNum = this.state.randomNum;
-    }
+    let query = `san diego`;
 
-    let query = `people/${randomNum}/`;
-    if (this.state.wookieFormat) {
-      query = query + "?format=wookiee";
-    }
-    // console.log('query string', query);
-    axios.get(`https://swapi.co/api/${query}`)
+    // fetch(`https://www.metaweather.com/api/location/search/?query=${query}`)
+    axios.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
     .then((res) => {
-      this.setState({ data: res.data, randomNum });
+      console.log('ressponse is: ', res);
+      this.setState({
+        weather: res.data
+       });
     })
     .catch(function (error) {
       console.error(error);
@@ -71,47 +65,38 @@ class Test extends Component {
     });
   }
 
-  mapWookieVersion(character) { // can do this since only 4 options... (not for large scale obviously)
-    switch (character) {
-    case "Lhuorwo Sorroohraanorworc":
-      return "luke_skywalker";
-    case "C-3PO":
-      return "c-3po";
-    case "R2-D2":
-      return "r2-d2";
-    case "Drarcaoac Vrawaworc":
-      return "darth_vader";
-    default:
-      return "character_image";
-      break;
-    }
-  }
 
 
   render() {
     let character = ""; // this.state.data && (this.state.data.name || this.state.data.whrascwo);
     const { data } = this.state;
 
-    if (data && data.name) {
-      character = data.name.toLowerCase().replace(" ", "_");
-    } else if (data && data.whrascwo) {
-      character = this.mapWookieVersion(data.whrascwo);
-    }
 
     return (
-      <div id="test-div">
+      <div id='test-div'>
         <h1>Test: (with characters 1-4)</h1>
 
-        <button onClick={this.refresh}>Call Swapi again</button>
+        {/* <button onClick={this.refresh}>Call Swapi again</button> */}
 
-        <button onClick={this.onWookieeVersionClick}>Change to Wookiee version</button>
+        <h3>Response from swapi for character {this.state.randomNum}:</h3>
+        <pre>
+          <code>
+            {JSON.stringify(this.state.weather, null, 4)}
+          </code>
+        </pre>
 
+
+        {/* eslint-disable react/jsx-indent */}
       { this.state.data && character && (
         <div>
-          <Image src={`/images/${character}.png`} responsive thumbnail alt={`Image for ${character}`} />
+          {/* <Image src={`/images/${character}.png`} responsive thumbnail alt={`Image for ${character}`} /> */}
 
           <h3>Response from swapi for character {this.state.randomNum}:</h3>
-          <pre><code>{JSON.stringify(this.state.data, null, 4)}</code></pre>
+          <pre>
+              <code>
+                {JSON.stringify(this.state.weather, null, 4)}
+              </code>
+            </pre>
 
         </div>
       ) || <Spinner1 />
