b36a430087e765f4a41f89222889031cd5be061f
nick <nick.bouldien@gmail.com>
2018-02-10 10:52:09 -0600

    redo some styles in about

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,31 +1,44 @@
 import React from 'react';
+import { List } from 'antd';
+
+const techData = [
+  <a href='https://github.com/facebook/react' target='_blank' rel='noopener noreferrer' >React 16</a>,
+  <a href='https://webpack.github.io/' target='_blank' rel='noopener noreferrer' >Webpack 3</a>,
+  <a href='https://ant.design/' target='_blank' rel='noopener noreferrer' >Ant Design</a>,
+  <a href='https://redux.js.org/' target='_blank' rel='noopener noreferrer' >Redux</a>,
+  <a href='https://github.com/gaearon/redux-thunk' target='_blank' rel='noopener noreferrer' >Redux-thunk</a>,
+  <a href='https://github.com/react-dnd/react-dnd' target='_blank' rel='noopener noreferrer' >React-DnD</a>
+];
+
+const apiData = [
+  <a href='https://iextrading.com/developer/'>IEX Trading (api used for stocks)</a>,
+  <a href='http://fixer.io/'>fixer.io (api used for currencies)</a>,
+  <a href='https://github.com/toddmotto/public-apis#currency-exchange'>github link</a>
+];
 
 const About = () => (
   <div id='about-div'>
     <h1>About this project:</h1>
-    <h3>Tech used:</h3>
-    <div>
-      <p>- <a href='https://github.com/facebook/react'>React 16</a></p>
-      <p>- <a href='https://webpack.github.io/'>Webpack 3</a> (too much fun to setup...)</p>
-      <p>- <a href='https://react-bootstrap.github.io/'>React Bootstrap</a> (quick styling)</p>
 
-    </div>
+    <h3 style={{ margin: '16px 0' }}>Tech Used:</h3>
 
-    <div>
-      API usage:
-      <p>- <a href='https://iextrading.com/api-exhibit-a'>IEX Trading (api used for stocks)</a></p>
-      <p>- <a href='http://fixer.io/'>fixer.io (api used for currencies)</a></p>
+    <List
+      size={"large"}
+      bordered
+      dataSource={techData}
+      renderItem={item => (<List.Item>{item}</List.Item>)}
+    />
 
-      For list of public apis:
-      <p>- <a href='https://github.com/toddmotto/public-apis#currency-exchange'>github link</a></p>
+    <h3 style={{ margin: '16px 0' }}>API usage:</h3>
 
-
-    </div>
-
-
+    <List
+      size={"large"}
+      bordered
+      dataSource={apiData}
+      renderItem={item => (<List.Item>{item}</List.Item>)}
+    />
 
   </div>
 );
-
 
 export default About;
